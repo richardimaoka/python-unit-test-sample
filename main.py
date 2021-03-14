66bfdcb9a070eae0d83d6c8ff2b6f2c66eddedb6
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-03-14 18:33:56 +0900

    test case from python jissen nyumon

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+def booksearch():
+  return ()
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
-if __name__ == '__main__':
-    unittest.main()
+class BookSearchTest(unittest.TestCase):
+  def test_booksearch(self):
+    self.assertEqual((), booksearch())
