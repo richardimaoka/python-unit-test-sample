e7edab5945541f38660cebfeba875d86eecf61bc
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-03-14 22:06:48 +0900

    fix errors in the book

--- booksearch/__init__.py
+++ booksearch/__init__.py
@@ -1,2 +1,3 @@
 from .core import Book, get_books
-__all__ = ['Book', 'get_books']
+from .api import build_url
+__all__ = ['Book', 'get_books', 'build_url']
--- booksearch/api.py
+++ booksearch/api.py
@@ -15,4 +15,4 @@ def get_data(url):
 
 def build_url(param):
     query = parse.urlencode(param)
-    return ('https://www.googleapis.com', f'/books/v1/volumes?{query}')
+    return 'https://www.googleapis.com' + f'/books/v1/volumes?{query}'
