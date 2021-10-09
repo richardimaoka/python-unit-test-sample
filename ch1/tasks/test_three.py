336c6cb7aa3e2658ce115b17a2cf3ae75e7b8095
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-09 21:08:35 +0900

    namedtuple

--- ch1/tasks/test_three.py
+++ ch1/tasks/test_three.py
@@ -3,6 +3,7 @@
 from collections import namedtuple
 
 Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
+# Task.__new__.__defaults__ = (None, None, False, None)
 
 
 def test_defaults():
