949c6ffd23d66c978f3e2d5e48db343df18e14db
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-09 22:15:49 +0900

    another way to pass default arguments to namedtuple

--- ch1/tasks/test_three.py
+++ ch1/tasks/test_three.py
@@ -2,7 +2,7 @@
 
 from collections import namedtuple
 
-Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
+Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'], defaults=[None, None, False, None])
 # Task.__new__.__defaults__ = (None, None, False, None)
 
 
