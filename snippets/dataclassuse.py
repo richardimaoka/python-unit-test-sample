21a96fa6fd4f86df79c2c7a0d20c87a6a5a44031
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-09 21:36:20 +0900

    dataclass miimck

--- snippets/dataclassuse.py
+++ snippets/dataclassuse.py
@@ -14,6 +14,10 @@ class RegularCard:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-
+    def __repr__(self):
+        return (f'{self.__class__.__name__}'
+                f'(rank={self.rank!r}, suit={self.suit!r})')
+                
 queen_of_hearts = RegularCard('Q', 'Hearts')
 print(queen_of_hearts.rank)
+print(queen_of_hearts)
