b4927f3bcead39e5478a7b7d1baa1f81cb58fa49
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-09 21:14:29 +0900

    mutation of list

--- ch1/collections.py
+++ ch1/collections.py
@@ -1,3 +1,13 @@
 items = ['note', 'notebook', 'sketchbook']
 print(type(items))
 print(items)
+
+items.append('paperbook')
+items = ['book'] + items
+print(items)
+
+items[3] = 'booklet'
+print(items)
+
+del items[1]
+print(items)
