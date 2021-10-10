85ae85610f02aec55e08ab7b653a166c9e3706f4
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-11 00:05:30 +0900

    create the tasks package

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name="ppkg", packages=find_packages())
+setup(
+    name="ppkg",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},  # root package is under the 'src' directory
+)
