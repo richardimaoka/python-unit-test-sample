0afb03ee7cef078697e7cda22ab302dfb5ae9da6
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-11 21:08:33 +0900

    default values for task

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,3 +1,5 @@
 from collections import namedtuple
 
-Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
+Task = namedtuple('Task',
+                  ['summary', 'owner', 'done', 'id'],
+                  defaults=(None, None, False, None))
