ffec5587abf741720b691ba9d90acd1eae964857
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-20 23:33:02 +0900

    compelte the file

--- ch4/test_tmpdir.py
+++ ch4/test_tmpdir.py
@@ -1,2 +1,9 @@
 def test_tmpdir(tmpdir):
     a_file = tmpdir.join('something.txt')
+    a_sub_dir = tmpdir.mkdir('anything')
+    another_file = a_sub_dir.join('something_else.txt')
+    a_file.write('contents may settle during shipping')
+    another_file.write('something different')
+
+    assert a_file.read() == 'contents may settle during shipping'
+    assert another_file.read() == 'something different'
