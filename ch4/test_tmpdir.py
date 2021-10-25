6f8cc149e9679263b6dc494b8667a9094cd0490f
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-26 05:50:10 +0900

    ch4 second test

--- ch4/test_tmpdir.py
+++ ch4/test_tmpdir.py
@@ -7,3 +7,20 @@ def test_tmpdir(tmpdir):
 
     assert a_file.read() == 'contents may settle during shipping'
     assert another_file.read() == 'something different'
+
+
+def test_tmpdir_factory(tmpdir_factory):
+    a_dir = tmpdir_factory.mktemp('mydir')
+
+    base_tmp = tmpdir_factory.getbasetemp()
+    print('base:', base_tmp)
+
+    a_file = a_dir.join('something.txt')
+    a_sub_dir = a_dir.mkdir('anything')
+    another_file = a_sub_dir.join('something_else.txt')
+
+    a_file.write('contents may settle during shipping')
+    another_file.write('something different')
+
+    assert a_file.read() == 'contents may settle during shipping'
+    assert another_file.read() == 'something different'
